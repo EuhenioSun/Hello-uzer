8d1d00faf209482bc2fe18e0da56f4cfaf55a9a7
Женя <esamoilov@yandex.ru>
2023-12-02 00:50:40 +0300

    добавили Привет и Еще раз

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите ваше имя ");
+
+string username = Console.ReadLine();
+
+ Console.Write ("Привет, ");
+ Console.WriteLine (username);
+ Console.Write ("Еще раз, ");
+ Console.WriteLine (username);
